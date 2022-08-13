78afa719aa78c3e72df065d2e28ac3a33e8c4b8b
Joanna Bell <jjbdigital@gmail.com>
2022-08-13 17:43:45 -0500

    commit tree faf3566e45f6d58de58e4bc01ebe0bd24f0335d8

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const buttonClick = document.getElementById('button')
+
 function addingEventListener() {
+  buttonClick.addEventListener('click', clicked)
+}
+
+function clicked() {
+  alert('I was clicked by addingEventListener')
 }
